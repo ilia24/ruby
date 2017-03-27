415d1a68de1ada68d0d75500caf409761c9b4c27
ilia24 <iliademertchian24@gmail.com>
2017-03-27 14:06:48 -0400

    Complete exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -8,5 +8,7 @@ puts calculatedcost
 value =  "lmao".to_i + 5
 puts value
 # exercise 3
-
+puts "the result of multiplying 45628 and 7839 is #{45628 * 7839}"
 # exercise 4
+(10 < 20 && 30 < 20) || !(10 == 11)
+#the value of the above would be true as || means only one size needs to be true to output as true, and !(10==11) outputs true
