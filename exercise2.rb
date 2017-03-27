cba2c0a1dbe0e614a83780a687b27a5c89fac198
ilia24 <iliademertchian24@gmail.com>
2017-03-27 14:03:29 -0400

    Complete questions 1/2

--- exercise2.rb
+++ exercise2.rb
@@ -1,7 +1,12 @@
 # exercise 1
 
-# exercise 2
+mealcost = 55
+calculatedcost = mealcost * 1.15
+puts calculatedcost
 
+# exercise 2
+value =  "lmao".to_i + 5
+puts value
 # exercise 3
 
 # exercise 4
