cac2c072f6815e34f720d41e2dbebea40a0f721c
ilia24 <iliademertchian24@gmail.com>
2017-03-27 15:46:44 -0400

    Add running/walk loop script with energy & distance tracking

--- exercise6.rb
+++ exercise6.rb
@@ -12,3 +12,35 @@ elsif baseenergy == "tired"
   energy += 2
 end
 end
+
+distance = 0
+homestatus = 0
+energystatus = 1
+
+while (homestatus == 0) && (energystatus == 1)
+  puts "Are you running or walking?"
+  speed = gets.chomp
+if energy <= 0
+  energystatus -= 1
+  puts "you are out of energy"
+elsif speed == "running"
+  distance += 5
+  energy -= 2
+  puts "You are currently #{distance}km from home, type home to stop"
+elsif speed == "run"
+  distance += 5
+  energy -=2
+  puts "You are currently #{distance}km from home, type home to stop egergy #{energy}"
+elsif speed == "walking"
+  distance += 1
+  energy += 1
+  puts "You are currently #{distance}km from home, type home to stop"
+elsif speed == "walk"
+  distance += 1
+  energy += 1
+  puts "You are currently #{distance}km from home, type home to stop"
+elsif speed == "home"
+  homestatus += 1
+  puts "You are currently #{distance} kilometres from home, good luck!"
+end
+end
